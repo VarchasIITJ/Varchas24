63ff80c8e563a8a8f9c0448dd95244f61f5447be
Saurabhrajmane-23 <iamsaurabhrajmane@gmail.com>
2024-09-12 15:27:22 +0530

    deployment update

--- client/src/components/CollegeModel.jsx
+++ client/src/components/CollegeModel.jsx
@@ -52,7 +52,7 @@ export default function CollegeModel() {
       }}
       onWheel={(event) => event.stopPropagation()}
     >
-      <Model url="assets/clg model9.glb" setIsHovering={setIsHovering} />
+      <Model url="public\clg model10.glb" setIsHovering={setIsHovering} />
       <OrbitControls
         enableZoom={isHovering}
         minDistance={120}
