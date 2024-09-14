e882a02d0018adfef2173d8bc8db1468c1db2e35
Chinmay Vashishth <b22bb016@iitj.ac.in>
2024-09-14 21:39:08 +0530

    added lazy loading and Not-Found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,57 +1,60 @@
-import Navbar from "./components/Navbar";
-import Events from "./pages/Events";
-import Sponsors from "./pages/Sponsors";
-import Team from "./pages/Team";
-import Home from "./pages/Home";
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
-import TeamCreate from "./pages/CreateTeam";
-import TeamJoin from "./pages/Join";
-import Payment from "./pages/Payment";
-import VTeam from "./pages/viewTeam";
-import Discount from "./pages/Discount";
-import Forgot from "./pages/Forgot";
-import Not_Found from "./pages/Not_Found";
-import SignUp from "./pages/Signup";
-import Form from "./pages/Form"
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
+// Lazy load other components
+const Events = lazy(() => import("./pages/Events"));
+const Sponsors = lazy(() => import("./pages/Sponsors"));
+const Team = lazy(() => import("./pages/Team"));
+const Login = lazy(() => import("./pages/Login"));
+const Profile = lazy(() => import("./pages/Profile"));
+const TeamCreate = lazy(() => import("./pages/CreateTeam"));
+const TeamJoin = lazy(() => import("./pages/Join"));
+const Payment = lazy(() => import("./pages/Payment"));
+const VTeam = lazy(() => import("./pages/viewTeam"));
+const Discount = lazy(() => import("./pages/Discount"));
+const Forgot = lazy(() => import("./pages/Forgot"));
+const SignUp = lazy(() => import("./pages/Signup"));
+const Form = lazy(() => import("./pages/Form"));
+const Not_Found = lazy(() => import("./pages/Not_Found"));
 
 const App = () => {
-
   return (
-
     <div>
       <div className="relative z-0 bg-black bg-cover bg-no-repeat bg-center">
         <Router>
-          <div className="">
+          <div>
             <Navbar />
-
           </div>
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/sponsors" element={<Sponsors />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/team" element={<Team />} />
+          {/* Suspense wraps lazy-loaded components */}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/sponsors" element={<Sponsors />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/team" element={<Team />} />
 
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/form" element={<Form/>} />
-            
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/form" element={<Form />} />
 
-            <Route path="/create" element={<TeamCreate />} />
-            <Route path="/join" element={<TeamJoin />} />
-            <Route path="/payment" element={<Payment />} />
-            <Route path="/view_team" element={<VTeam />} />
+              <Route path="/create" element={<TeamCreate />} />
+              <Route path="/join" element={<TeamJoin />} />
+              <Route path="/payment" element={<Payment />} />
+              <Route path="/view_team" element={<VTeam />} />
 
-            <Route path="/discount" element={<Discount />} />
-            <Route path="/forgot" element={<Forgot />} />
+              <Route path="/discount" element={<Discount />} />
+              <Route path="/forgot" element={<Forgot />} />
 
-          </Routes>
+              {/* Catch-all route for undefined paths */}
+              <Route path="*" element={<Not_Found />} />
+            </Routes>
+          </Suspense>
         </Router>
-      </div >
+      </div>
     </div>
   );
 };
