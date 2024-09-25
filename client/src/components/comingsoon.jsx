53939d393f3ae73baacab3dbc79d4b160fb6aab2
Saurabhrajmane-23 <iamsaurabhrajmane@gmail.com>
2024-09-25 23:07:53 +0530

    updated payment page

--- client/src/components/comingsoon.jsx
+++ client/src/components/comingsoon.jsx
@@ -1,8 +1,8 @@
 const Comingsoon = () => {
   return (
     <main className="relative w-full h-screen mx-auto sm:px-16 px-6 sm:py-16 py-10 max-w-7xl z-0 flex flex-col items-center justify-center">
-      <div className="text-emerald-500 mt-7 text-[100px] font-Londrina_sketch text-center">
-        Coming Soon
+      <div className="text-yellow-400 mt-7 text-[100px] font-sans text-center">
+        Coming Soon!!
       </div>
     </main>
   );
