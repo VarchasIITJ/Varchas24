b03ce8a8f674f9d1644acb0588f3bea944be2ef5
Chinmay Vashishth <b22bb016@iitj.ac.in>
2024-09-25 20:49:59 +0530

    Converted Singles to Individual in Team Options

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -765,7 +765,7 @@ export const eventOptions = [
     value: "2",
     label: "Badminton",
     category: ["men", "women", "mixed"],
-    teamTypes: ["Team","Single"],
+    teamTypes: ["Team","Individual"],
   },
   {
     value: "3",
@@ -789,13 +789,13 @@ export const eventOptions = [
     value: "6",
     label: "Table Tennis",
     category: ["men", "women"],
-    teamTypes: ["Single"],
+    teamTypes: ["Individual"],
   },
   {
     value: "7",
     label: "Lawn Tennis",
     category: "men",
-    teamTypes: ["Single"],
+    teamTypes: ["Individual"],
   },
   {
     value: "8",
@@ -819,7 +819,7 @@ export const eventOptions = [
     value: "11",
     label: "Squash",
     category: "men",
-    teamTypes: ["Single"],
+    teamTypes: ["Individual"],
   },
   {
     value: "12",
@@ -937,7 +937,7 @@ export const eventOptions = [
     value: "15",
     label: "Clash Royale",
     category: "open",
-    teamTypes: ["Single"],
+    teamTypes: ["Individual"],
     team_id: [
       {
         labelText: "id1",
@@ -964,7 +964,13 @@ export const teamTypeOptions = [
   "4x100m",
   "4x400m",
   "Team",
-  "Single",
+  "Individual",
+  "Long Jump",
+  "Triple Jump",
+  "High Jump",
+  "Discuss Throw",
+  "Javelin Throw",
+  "Shot Put" 
 ];
 
 export { events, signupFields, loginFields, addInfoFields };
