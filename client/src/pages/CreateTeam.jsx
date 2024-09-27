dc8dab7244445e3910280f1cc78a18ce1054921e
Chinmay Vashishth <b22bb016@iitj.ac.in>
2024-09-27 13:26:18 +0530

    Opened Women Segment in Kabaddi and Updated Endpoint in Forget.jsx

--- client/src/pages/CreateTeam.jsx
+++ client/src/pages/CreateTeam.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/header";
 import FormAction from "../components/formaction";
-import { eventOptions } from "../constants";
+import { eventOptions, teamTypeOptions } from "../constants";
 import Select from "react-select";
 import Input from "../components/input";
 import axios from "axios";
