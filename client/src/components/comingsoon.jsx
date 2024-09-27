8653f0ed84bb340dd6d115b4551c702417a62511
Saurabhrajmane-23 <iamsaurabhrajmane@gmail.com>
2024-09-27 18:35:16 +0530

    updates

--- client/src/components/comingsoon.jsx
+++ client/src/components/comingsoon.jsx
@@ -1,7 +1,7 @@
 const Comingsoon = () => {
   return (
     <main className="relative w-full h-screen mx-auto sm:px-16 px-6 sm:py-16 py-10 max-w-7xl z-0 flex flex-col items-center justify-center">
-      <div className="text-yellow-400 mt-7 text-[100px] font-sans text-center">
+      <div className="text-yellow-400 mt-7 text-[70px] font-sans text-center">
         Coming Soon!!
       </div>
     </main>
